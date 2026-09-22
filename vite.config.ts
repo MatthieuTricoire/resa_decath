1b8ed5518cbee638a1797129f876edbaa2caa5fc
Matth <matth@mbp-de-matth2.home>
2026-09-22 11:11:14 +0200

    remove nitro

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
@@ -13,21 +12,10 @@ const config = defineConfig({
 	},
 	build: {
 		rolldownOptions: {
-			external: [/^kysely$/, /^@better-auth\/kysely-adapter$/],
+			external: [/^@sentry\//, /^kysely$/, /^@better-auth\/kysely-adapter$/],
 		},
 	},
-	plugins: [
-		devtools(),
-		nitro({
-			rollupConfig: {
-				external: [/^@sentry\//, /^kysely$/, /^@better-auth\/kysely-adapter$/],
-			},
-		}),
-		tailwindcss(),
-		tanstackStart(),
-		viteReact(),
-		netlify(),
-	],
+	plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(), netlify()],
 });
 
 export default config;
