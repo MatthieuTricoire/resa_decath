4b41bee01dca411475ecea58ccc5ffbdde65cb08
Matth <matth@mbp-de-matth2.home>
2026-09-22 11:05:01 +0200

    add netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
-
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
@@ -27,6 +26,7 @@ const config = defineConfig({
 		tailwindcss(),
 		tanstackStart(),
 		viteReact(),
+		netlify(),
 	],
 });
 
