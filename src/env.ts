c0aa981a6749fd2fd2a025a8a707d64c46ef0853
Matth <matth@mbp-de-matth2.home>
2026-09-22 11:46:27 +0200

    update env.ts

--- src/env.ts
+++ src/env.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 
 export const env = createEnv({
 	server: {
-		DATABASE_URL: z.string().url(),
+		DATABASE_URL: z.string().min(1),
 		NODE_ENV: z
 			.enum(["development", "test", "production"])
 			.default("production"),
 		BETTER_AUTH_SECRET: z.string().min(32),
-		BETTER_AUTH_URL: z.string().url(),
+		BETTER_AUTH_URL: z.string().min(1),
 	},
 	clientPrefix: "VITE_",
 	client: {
